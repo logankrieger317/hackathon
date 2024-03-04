809289ea4cb3077f5a041f8e275d77a70e66637d
Logan <logankrieger317@gmail.com>
2024-03-04 14:48:30 -0600

    Login & Signup Form, Footer Rmv, Nav Icons Created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import  "./App.css"
 import Header from './components/Header'
 import Footer from "./components/Footer"
 import Home from "./components/Home";
-// import { Auth } from "./components/auth";
+import Login from "./components/Login";
+
 
 function App() {
   return (
     <div className="App"> 
     
       <div className="login">
-      {/* <Auth /> */}
-      <Home/>
+      <Login />
+      {/* <Home/> */}
       </div>
     
     </div>
