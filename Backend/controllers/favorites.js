2f73df118dfb4544abf7659090bbd3b4ead3cb2f
SelamB <selambeyene2@gmail.com>
2024-03-04 13:30:00 -0800

    tests index fav api

--- Backend/controllers/favorites.js
+++ Backend/controllers/favorites.js
@@ -7,16 +7,34 @@ module.exports = {
     unfavorite
 }
 
-//Shows all of a user's favorite plants
+// Shows all of a user's favorite plants
+// Pass userId in request body
 async function index(req, res) {
     console.log('Index favs controller hit')
     try {
         // pull the userId
+        const {userId} = req.body
         // findbyId
+        const user = await User.findById(userId)
+        // check if user is found
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
         // get user.favorites
+        const favoritedIds = user.favorites //returns an array of fav IDs
+        // check if user has favorited plants
+        if (favoritedIds.length === 0) {
+            return res.status(200).json({ message: "No favorite plants found for the user" });
+        }
+        // retrieve full details of favorited plants
+        const favoritedPlants = await Promise.all(favoritedIds.map(async (favoriteId) => {
+            return await Favorite.findById(favoriteId);
+        }));
         // send that array to frontend
+        res.json(favoritedPlants)
     } catch (err) {
-        
+        console.error('Error in index favs controller:', err);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
 
